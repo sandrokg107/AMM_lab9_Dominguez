d62eb54e3a627dc304f15c6dbf26c9d5ef12b625
sandrokg107 <sandrokg107@gmai.com>
2023-11-18 21:21:12 -0500

    Agregar archivos de proyecto.

---
+++ AMM_lab9_Dominguez/AMM_lab9_Dominguez/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AMM_lab9_Dominguez
+{
+    public partial class App : Application
+    {
+        public static float ScreenHeight {  get; set; }
+        public static float ScreenWidth { get; set; }
+        public App()
+        {
+            InitializeComponent();
+
+            
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
